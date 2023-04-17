fca884bca77f0e5a3a0efe755bbd0103a53ff60b
Nadiia Tsytsylina <miniova95@gmail.com>
2023-04-17 20:00:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,56 @@
-// import './css/styles.css';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const KEY = '35495478-0f618b27834e323a3a3099cd4';
 const form = document.querySelector('.search-form');
 const input = document.querySelector('input');
-const button = document.querySelector('.submit-button');
 const gallery = document.querySelector('.gallery');
+const buttonMore = document.querySelector('.load-more');
+const KEY = '35495478-0f618b27834e323a3a3099cd4';
+const perPage = 40;
+let page = 1;
 
-function fetchImages(name) {
-  return fetch(
-    `https://pixabay.com/api/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`
-  ).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  });
-}
+buttonMore.classList.add('hide');
 
 form.addEventListener('submit', onSubmit);
+buttonMore.addEventListener('click', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
   const name = input.value;
   fetchImages(name)
     .then(data => {
-      if (data.hits.length === 0) {
-        console.log(
-          'Sorry, there are no images matching your search query. Please try again.'
+      const totalPages = Math.ceil(data?.totalHits / perPage);
+      if (e.type === 'submit') {
+        gallery.innerHTML = '';
+        page = 1;
+        if (data.hits.length === 0) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          Notify.success(`Hooray! We found ${data.totalHits} images`);
+          buttonMore.classList.remove('hide');
+        }
+      } else if (page >= totalPages) {
+        buttonMore.classList.add('hide');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
         );
-      } else {
-        console.log(data.hits);
-        insertContent(data.hits);
       }
+      insertContent(data.hits);
+      page += 1;
     })
     .catch(error => console.log(error));
+}
+
+function fetchImages(name) {
+  return fetch(
+    `https://pixabay.com/api/?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${page}`
+  ).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  });
 }
 
 function createPhotoCard(item) {
@@ -61,80 +77,7 @@ function createPhotoCard(item) {
 </div>`;
 }
 
-function generatePhotos(array) {
-  return array.reduce((acc, item) => acc + createPhotoCard(item), '');
-}
-
 function insertContent(array) {
-  const result = generatePhotos(array);
-  return (gallery.innerHTML = result);
+  const result = array.reduce((acc, item) => acc + createPhotoCard(item), '');
+  return gallery.insertAdjacentHTML('beforeend', result);
 }
-
-// const DEBOUNCE_DELAY = 300;
-// const searchBox = document.querySelector('#search-box');
-// const countryList = document.querySelector('.country-list');
-// const countryInfo = document.querySelector('.country-info');
-
-// searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
-// function onInput(event) {
-//   event.preventDefault();
-//   if (event.target.value === '') {
-//     return;
-//   } else {
-//     fetchCountries(searchBox.value.trim())
-//       .then(data => {
-//         if (data.length > 10) {
-//           clearHtml(countryList);
-//           clearHtml(countryInfo);
-//           Notify.info(
-//             'Too many matches found. Please enter a more specific name.'
-//           );
-//         } else if (data.length === 1) {
-//           clearHtml(countryList);
-//           createCountryCard(data[0]);
-//         } else {
-//           clearHtml(countryInfo);
-//           insertContent(data);
-//         }
-//       })
-//       .catch(error => {
-//         Notify.failure('Oops, there is no country with that name');
-//         clearHtml(countryList);
-//         clearHtml(countryInfo);
-//       });
-//   }
-// }
-
-// function clearHtml(element) {
-//   return (element.innerHTML = '');
-// }
-
-// function createLi(item) {
-//   return `<li class = 'item'>
-//     <img class='item-img' src='${item.flags.svg}' alt = "flag of ${item.flags.alt}">
-//     <p class = 'item-descr'>${item.name.official}</p>
-//   </li>`;
-// }
-
-// function generateCountries(array) {
-//   return array.reduce((acc, item) => acc + createLi(item), '');
-// }
-
-// function insertContent(array) {
-//   const result = generateCountries(array);
-//   return (countryList.innerHTML = result);
-// }
-
-// function createCountryCard(item) {
-//   const languagesList = Object.values(item.languages);
-//   return (countryInfo.innerHTML = `
-//   <div class ='country-card'>
-//   <img class='card-img' src='${item.flags.svg}' alt = "flag of ${item.flags.alt}">
-//     <h2 class = 'card-name'>${item.name.official}</h2>
-//     </div>
-//     <p class = 'card-descr'><span class = 'card-subtitle'>Capital:</span>${item.capital}</p>
-//     <p class = 'card-descr'><span class = 'card-subtitle'>Population:</span>${item.population}</p>
-//     <p class = 'card-descr'><span class = 'card-subtitle'>Languages:</span>${languagesList}
-//     </p>`);
-// }
